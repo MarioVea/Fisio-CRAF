6d5e89572176151c7661f40d85cc91cbc045cbcc
Mario Soto Vea <23030868@uadeo.mx>
2026-04-22 09:55:55 -0700

    Commit inicial

---
+++ FisioCRAF/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using FisioCRAF.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FisioCRAF.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FisioCRAF/Models/Conexion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace FisioCRAF.Models
+{
+    public class Conexion
+    {
+        public string conexion()
+        {
+            string conex = "";
+            string rutacompleta = @"C:\conexion\FisioCRAF.txt";
+            using (StreamReader file = new StreamReader(rutacompleta))
+            {
+                conex = @"" + file.ReadToEnd();
+                file.Close();
+            }
+            return conex;
+        }
+    }
+}
